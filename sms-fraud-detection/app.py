edd0df95f00535c1add2f03afb2c6c7cb9313511
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-11-05 15:39:46 +0530

    Update app.py

--- sms-fraud-detection/app.py
+++ sms-fraud-detection/app.py
@@ -9,6 +9,12 @@ from nltk.stem.porter import PorterStemmer
 import warnings
 warnings.filterwarnings('ignore')
 
+# Download required NLTK resources if not already downloaded
+nltk.download('punkt')     # This is essential for tokenization
+nltk.download('stopwords')  # Download stopwords if not already downloaded
+nltk.download('punkt_tab')
+
+# Connect to the PostgreSQL database
 hostname = 'dpg-csksslu8ii6s7380o05g-a.oregon-postgres.render.com'
 port = 5432
 database = 'fraud_detection_binary_files'
@@ -18,31 +24,25 @@ password = 'p3Qtz54g2Btije8jb4BCjuEKJWSoGyTA'
 connection = psycopg2.connect(host=hostname, port=port, database=database, user=username, password=password)
 cursor = connection.cursor()
 
-
 st.title("Email/SMS Spam Classifier")
-
-nltk.download('stopwords')
-nltk.download('punkt')
 
 ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-    
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
 
+    y = [i for i in text if i.isalnum()]
     text = y[:]
     y.clear()
+    
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
 
     text = y[:]
     y.clear()
+    
     for i in text:
         y.append(ps.stem(i))
 
@@ -56,29 +56,31 @@ def retrieve_model(model_name):
 def load_model_from_db(model_name):
     model_data = retrieve_model(model_name)
     if model_data:
-        return pickle.loads(model_data)  
+        return pickle.loads(model_data)
     else:
         print(f"{model_name} not found in the database.")
         return None
 
 # Load models from the database
-tfidf = load_model_from_db(r'C:\Users\saura\OneDrive\Documents\GitHub\SMS-fraud-detection\sms-fraud-detection\vectorizer.pkl')
-model = load_model_from_db(r'C:\Users\saura\OneDrive\Documents\GitHub\SMS-fraud-detection\sms-fraud-detection\model.pkl')
-
+tfidf = load_model_from_db('vectorizer')  # Use the model name
+model = load_model_from_db('model')         # Use the model name
 
 input_sms = st.text_area('Enter the message')
 
 if st.button('Predict'):
-    transformed_sms = transform_text(input_sms)
+    if tfidf and model:  # Ensure models are loaded
+        transformed_sms = transform_text(input_sms)
+        vector_inp = tfidf.transform([transformed_sms])
+        
+        result = model.predict(vector_inp)[0]
 
-    vector_inp = tfidf.transform([transformed_sms])
-
-    result = model.predict(vector_inp)[0]
-
-    if result == 1:
-        st.header('Spam')
+        if result == 1:
+            st.header('Spam')
+        else:
+            st.header('Not Spam')
     else:
-        st.header('Not Spam')
+        st.error("Model loading failed. Unable to make predictions.")
 
+# Clean up
 cursor.close()
 connection.close()
